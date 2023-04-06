1e46c194a47948b098f08bc9c58e8f9ab0e0fa4c
Vsevolod9419 <smolin_9419@mail.ru>
2023-04-06 12:51:21 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+import { data } from "./emoji.js";
+
+let newData = []
+function anyFilter(arr){
+	for(let i = 0; i < arr.length; i++) {
+		newData.push({title: arr[i]['title'],
+		symbol: arr[i]['symbol'],
+		keywords: [...new Set(arr[i]["keywords"].split(' '))].join(' ')})
+	}
+}
+
+anyFilter(data);
+
+let container = document.querySelector(".container");
+
+function emojicard(obj) {
+  let card2 = document.createElement("div");
+  let a = document.createElement("p");
+  let b = document.createElement("p");
+  let c = document.createElement("p");
+
+  card2.prepend(a, b, c);
+  a.textContent = obj.symbol;
+  b.textContent = obj.title;
+  c.textContent = obj.keywords;
+
+  card2.setAttribute("class", "emoji_card");
+  a.setAttribute("class", "emoji");
+  b.setAttribute("class", "emoji_name");
+  c.setAttribute("class", "emoji_keyworsds");
+
+  container.append(card2);
+}
+
+newData.forEach(emojicard);
+
+let input = document.querySelector('input');
+
+input.addEventListener('input', search)
+
+function search(e) {
+	let value = e.target.value.toLowerCase().trim();
+	let filterEmoji =  newData.filter((e) => e.title.includes(input.value) || e.keywords.includes(input.value))
+	container.innerHTML = '';
+	filterEmoji.forEach(emojicard)
+}
+
